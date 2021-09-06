6dc55f04830c590e4647d914e8f7882cb78a45b9
Sharodh K R <61955784+krsharodh@users.noreply.github.com>
2021-09-06 15:33:36 +1000

    Adding Base Project

---
+++ DeveloperPracticalTest.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using DeveloperPracticalTest;
+
+namespace DeveloperPracticalTest.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
